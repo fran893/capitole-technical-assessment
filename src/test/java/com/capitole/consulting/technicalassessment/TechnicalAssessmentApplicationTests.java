764756a9d07b151e4d2cce269abd8326d4e85d42
fran893 <alvarez.fr893@gmail.com>
2022-01-19 12:59:29 -0300

    Commit inicial

---
+++ src/test/java/com/capitole/consulting/technicalassessment/TechnicalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.consulting.technicalassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
