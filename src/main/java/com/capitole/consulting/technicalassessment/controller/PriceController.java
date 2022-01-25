5b25a7faba0ca6e7034e2091b3af9d0166725a6d
Franco Alvarez <alvarez.fr893@gmail.com>
2022-01-24 22:42:59 -0300

    Remove unused imports, remove unnecessary annotations

--- src/main/java/com/capitole/consulting/technicalassessment/controller/PriceController.java
+++ src/main/java/com/capitole/consulting/technicalassessment/controller/PriceController.java
@@ -1,9 +1,7 @@
 package com.capitole.consulting.technicalassessment.controller;
 
-import com.capitole.consulting.technicalassessment.model.Price;
 import com.capitole.consulting.technicalassessment.model.PriceResult;
 import com.capitole.consulting.technicalassessment.service.IPriceService;
-import com.capitole.consulting.technicalassessment.utils.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -11,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -28,7 +25,6 @@ public class PriceController {
     }
 
     @GetMapping("/getPrices")
-    @ResponseBody
     public ResponseEntity<List<PriceResult>> getPrices(@RequestParam("applyDate")
                                             @DateTimeFormat(pattern = "yyyy-MM-dd-HH.mm.ss") LocalDateTime applyDate,
                                                       @RequestParam("productId") Integer productId,
@@ -39,7 +35,6 @@ public class PriceController {
         } else {
             throw new ResourceNotFoundException();
         }
-
     }
 
 }
