178852ae539a5679cf6dc6430211f8b6fae245c0
Franco Alvarez <alvarez.fr893@gmail.com>
2022-01-24 22:44:50 -0300

    Replace all imports with single imports

--- src/test/java/com/capitole/consulting/technicalassessment/integrations/controllers/PriceControllerIntegrationTest.java
+++ src/test/java/com/capitole/consulting/technicalassessment/integrations/controllers/PriceControllerIntegrationTest.java
@@ -9,7 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 
 @SpringBootTest(
